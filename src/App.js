678f762a5059035493d83222e585ffd534f0df72
Noemi Scotellaro <noemi.scotellaro@gmail.com>
2021-12-18 02:46:23 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs";
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap-utilities.min.css';
+import 'bootstrap/dist/css/bootstrap-reboot.min.css';
 
-function App() {
+const App = () => {
+  const buttons = [
+    "c",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "/",
+    "=",
+  ];
+  const [display, setDisplay] = useState("0");
+
+  const handler = (button) => {
+    if (button === "=") {
+      setDisplay(evaluate(display));
+    } else if (button === "c") {
+      setDisplay("0");
+    } else {
+      setDisplay(display + button);
+    }
+
+
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="modal-dialog border border-warning">
+
+      <h1 className="display-2 text-center text-warning ">{display}</h1>
+      <div className="container">
+          <div className="row">
+            {buttons.slice(0,1).map((button, index) => {
+                return <Button handler={handler} key={index} button={button} extraClass="col-12"  />;
+            })}
+          </div>
+
+          <div className="row">
+            {buttons.slice(1,5).map((button, index) => {
+                return <Button handler={handler} key={index} button={button} extraClass="col-3"/>;
+            })}
+          </div>
+          
+          <div className="row">
+            {buttons.slice(5,9).map((button, index) => {
+                return <Button handler={handler} key={index} button={button} extraClass="col-3" />;
+            })}
+          </div>
+
+          <div className="row">
+            {buttons.slice(9,13).map((button, index) => {
+                return <Button handler={handler} key={index} button={button} extraClass="col-3" />;
+            })}
+          </div>
+
+          <div className="row">
+            {buttons.slice(13,17).map((button, index) => {
+                return <Button handler={handler} key={index} button={button} extraClass="col-3" />;
+            })}
+          </div>
+      </div>
     </div>
   );
-}
+};
 
+const Button = (props) => {
+  return (
+      <div className={"d-grid " + props.extraClass}>
+          <button className="btn btn-outline-warning btn-lg" onClick={() => props.handler(props.button)}>{props.button}</button>
+      </div>
+  );
+};
 export default App;
